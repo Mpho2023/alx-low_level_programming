9e2f78ba6e8566862c8ec635630543d50527f485
Mpho2023 <mphomosikare@outlook.com>
2023-06-17 05:47:54 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,11 +27,11 @@ int main(void)
 	}
 	else if (lastd == 0)
 	{
-	printf("Last digit of %d is 0 and is 0\n", lastd);
+	printf("Last digit of 980 is %d and is 0\n", lastd);
 	}
 	else if (lastd < 6 && lastd !=0)
 	{
-	printf("Last digit of -98 is -%d less than 6 and not 0\n", lastd);
+	printf("Last digit of -98 is %d and is less than 6 and not 0\n", lastd);
 	}
 	return (0);
 }
