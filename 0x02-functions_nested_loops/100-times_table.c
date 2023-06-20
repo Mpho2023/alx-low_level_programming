ceefdfd2725901c74a0010b931e5b35a876804b7
Mpho2023 <mphomosikare@outlook.com>
2023-06-20 13:19:52 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,7 +8,6 @@
 	{
 	int i, j, k;
 
-
 	if (n >= 0 && n <= 15)
 	{
 	for (i = 0; i <= n; i++)
@@ -19,21 +18,24 @@
 	if (j == 0)
 	{
 	_putchar(k + '0');
-	} else if (k < 10 && j != 0)
+	}
+	else if (k < 10 && j != 0)
 	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar(k + '0');
-	} else if (k >= 10 && k < 100)
+	}
+	else if (k >= 10 && k < 100)
 	{
 	_putchar(',');
 	_putchar(' ');
 	_putchar(' ');
 	_putchar((k / 10) + '0');
 	_putchar((k % 10) + '0');
-	} else if (k >= 100)
+	}
+	else if (k >= 100)
 	{
 	_putchar(',');
 	_putchar(' ');
@@ -41,10 +43,5 @@
 	_putchar(((k / 10) % 10) + '0');
 	_putchar((k % 10) + '0');
 	}
-	}
 	_putchar('\n');
-	}
-	}
-	}
-
-
+	{
